0ed991186c014a5246c0a6084eb4ef42c27e5352
Guilherme Fernandes Pereira <o.guifernandes@gmail.com>
2022-06-22 09:37:54 -0300

    96. Sign In With Redirect

--- crwn-clothing/src/routes/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,24 @@
-import { createUserDocumentFromAuth, signInWithGooglePopup } from '../../utils/firebase.utils';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
+import {
+  auth,
+  createUserDocumentFromAuth,
+  signInWithGooglePopup,
+  signInWithGoogleRedirect,
+} from '../../utils/firebase.utils';
 
 const SignIn = () => {
+  useEffect(() => {
+    const getLoginRedirectResult = async () => {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        await createUserDocumentFromAuth(response.user);
+      }
+    }
+    getLoginRedirectResult();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -11,6 +29,9 @@ const SignIn = () => {
       <h1>Sign In Page.</h1>
       <button onClick={logGoogleUser}>
         Sign in with Google Popup
+      </button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
       </button>
     </div>
   );
