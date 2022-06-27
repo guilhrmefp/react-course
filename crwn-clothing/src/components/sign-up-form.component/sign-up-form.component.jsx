f535af1c85524e6b6624103ff87b1ecbf114a674
Guilherme Fernandes Pereira <o.guifernandes@gmail.com>
2022-06-27 08:44:42 -0300

    101. Custom Button Component

--- crwn-clothing/src/components/sign-up-form.component/sign-up-form.component.jsx
+++ crwn-clothing/src/components/sign-up-form.component/sign-up-form.component.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase.utils';
 
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+
+import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
   displayName: '',
@@ -45,8 +48,9 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <h1>Sign up with your e-mail and password.</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your e-mail and password.</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label="Display Name"
@@ -84,7 +88,9 @@ const SignUpForm = () => {
           onChange={handleChange}
         />
 
-        <button type="submit">Submit</button>
+        <Button type="submit">
+          Sign Up
+        </Button>
       </form>
     </div>
   );
