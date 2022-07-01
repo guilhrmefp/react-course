7770fba6ad44648e7d7e485d862f8af3d119fa60
Guilherme Fernandes Pereira <o.guifernandes@gmail.com>
2022-07-01 09:22:48 -0300

    Try it yourself: Create context for cart dropdown state (open/closed)

--- crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
+++ crwn-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -1,10 +1,22 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart-context';
+
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+
+  const handleOpenCart = () => {
+    setIsCartOpen(!isCartOpen);
+  };
+
   return (
-    <div className="cart-icon-container">
+    <div
+      className="cart-icon-container"
+      onClick={handleOpenCart}
+    >
       <ShoppingIcon className="shopping-icon" />
       <span className="item-count">0</span>
     </div>
