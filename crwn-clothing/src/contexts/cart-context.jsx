c7c2fcc0aa0f525e1c5bdaa3ce89d2d2eb34aa5b
Guilherme Fernandes Pereira <o.guifernandes@gmail.com>
2022-07-06 08:39:41 -0300

    117. Add To Cart Pt.1

--- crwn-clothing/src/contexts/cart-context.jsx
+++ crwn-clothing/src/contexts/cart-context.jsx
@@ -1,12 +1,28 @@
 import { useState, createContext } from 'react';
 
+const addCartItem = (cartItems, productToAdd) => {
+  // find if cartItems contains productToAdd
+
+  // if found, increment quantity
+
+  // return new array with modified cartItems / new cart item
+}
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({children}) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productTOAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  }
+
   const value = {
     isCartOpen,
     setIsCartOpen,
