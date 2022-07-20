e839b8229833777e7a142b4d706ede2f9d57195c
Guilherme Fernandes Pereira <o.guifernandes@gmail.com>
2022-07-20 08:48:52 -0300

    138. Styled-Component - Cart Dropdown

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,10 +2,10 @@ import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart-context';
 import { useNavigate } from 'react-router-dom';
 
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
-import './cart-dropdown.styles.scss';
+import { CartDropdownContainer, EmptyMessage, CartItems } from './cart-dropdown.styles';
 
 const CartDropdown = () => {
   const { cartItems, setIsCartOpen } = useContext(CartContext);
@@ -18,19 +18,19 @@ const CartDropdown = () => {
   };
 
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
+    <CartDropdownContainer>
+      <CartItems>
         {
-          cartItems.map((item) => (
+          cartItems.length ? cartItems.map((item) => (
             <CartItem key={item.id} cartItem={item} />
-          ))
+          )) : <EmptyMessage>Yout cart is empty</EmptyMessage>
         }
-      </div>
+      </CartItems>
 
       <Button onClick={goToCheckoutHandler}>
         Go to Checkout
       </Button>
-    </div>
+    </CartDropdownContainer>
   )
 };
 
