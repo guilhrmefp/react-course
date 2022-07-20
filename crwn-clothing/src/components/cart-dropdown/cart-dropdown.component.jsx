6483bb93b338c6be706c32cb69f18f25aff32536
Guilherme Fernandes Pereira <o.guifernandes@gmail.com>
2022-07-20 09:11:02 -0300

    139. Styled-Component - Directory Item & Cart Icon

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart-context';
 import { useNavigate } from 'react-router-dom';
 
-import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
+import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 import { CartDropdownContainer, EmptyMessage, CartItems } from './cart-dropdown.styles';
