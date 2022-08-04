ab060d09b634305d8bee7a62f9a0044ba8cd4e58
Guilherme Fernandes Pereira <o.guifernandes@gmail.com>
2022-08-04 09:01:41 -0300

    154. React-Redux: Selectors

--- crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,10 @@
 import { Fragment, useContext } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import { signOutUser } from '../../utils/firebase.utils';
-import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart-context';
+import { selectCurrentUser } from '../../store/user/user.selector';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
@@ -13,7 +14,7 @@ import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component
 import { NavigationContainer, LogoContainer, NavLinks, NavLink } from './navigation.styles';
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
   const { isCartOpen } = useContext(CartContext);
 
   const signOutHandler = async () => {
